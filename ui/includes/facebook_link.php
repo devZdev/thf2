df7dfe6ae0b5f92b01a18f89c8145b06dfbb169a
August Hall <augusthall@gmail.com>
2014-04-28 19:55:36 -0400

    commented out donate/facebook includes

--- ui/includes/facebook_link.php
+++ ui/includes/facebook_link.php
@@ -1,6 +1,6 @@
-<div class="span-22 prepend-2 last">
+<!-- <div class="span-22 prepend-2 last">
 	<div class="legal">
 		<a href="http://www.facebook.com/pages/Teenok-Heart-Foundation/140567699332485"><img src="/ui/images/thf_fb.jpg" alt="Find us on Facebook"></a>
 		<span class="tag">site by <span class="red"><a href="http://hoverspark.com">HOVER</span><span class="grey">SPARK</span></span></a>
 	</div>
-</div>
+</div> -->
